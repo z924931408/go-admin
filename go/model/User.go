4d6f307dfdabcd25a9d7cfd97b5d283e3dbfd7c9
zhujiqian <924931408@qq.com>
2021-07-15 07:21:31 +0800

    增加model

---
+++ go/model/User.go
@@ -0,0 +1,21 @@
+package model
+
+// 数据库表明自定义，默认为model的复数形式，比如这里默认为 users
+func (User) TableName() string {
+	return "sys_user"
+}
+
+type User struct {
+	Id int `json:"id"`
+	Name string `json:"name"` //用户名
+	NickName string `json:"nickName"`
+	Avatar string `json:"avatar"`
+	Password string `json:"password"`
+	Email string `json:"email"`
+	Mobile string `json:"mobile"`
+	DelStatus int `json:"delStatus"`
+	CreateTime int64 `json:"createTime"`
+}
+
+
+
