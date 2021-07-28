25adea3a1d36e2a61f7ef17542592b73c3081b51
zhujiqian <924931408@qq.com>
2021-07-28 22:38:31 +0800

    增加角色模块controller层

---
+++ go/controller/RoleController.go
@@ -0,0 +1,70 @@
+package controller
+
+import (
+	"go-admin/go/model"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"go-admin/go/service"
+)
+
+
+func CreateRole(c *gin.Context)  {
+	var user model.User
+	c.BindJSON(&user)
+	err:=service.CreateUser(&user)
+	if err!=nil{
+		c.JSON(http.StatusBadRequest,gin.H{"error":err.Error()})
+	}else {
+		c.JSON(http.StatusOK,gin.H{
+			"code":200,
+			"msg":"success",
+			"data":user,
+		})
+	}
+}
+
+
+func GetRoleList(c *gin.Context)  {
+	todoList,err :=service.GetAllUser()
+	if err!=nil{
+		c.JSON(http.StatusBadRequest,gin.H{"error":err.Error()})
+	}else {
+		c.JSON(http.StatusOK,gin.H{
+			"code":200,
+			"msg":"success",
+			"data":todoList,
+		})
+	}
+}
+
+func UpdateRole(c *gin.Context)  {
+	id,ok:=c.Params.Get("id")
+	if !ok{
+		c.JSON(http.StatusBadRequest,gin.H{"error":"无效的id"})
+	}
+	user,err:= service.GetUserById(id)
+	if err!=nil{
+		c.JSON(http.StatusOK,gin.H{"error":err.Error()})
+		return
+	}
+	c.BindJSON(&user)
+	if err= service.UpdateUser(user);err!=nil{
+		c.JSON(http.StatusBadRequest,gin.H{"error":err.Error()})
+	}else {
+		c.JSON(http.StatusOK,user)
+	}
+}
+
+
+
+func DeleteRoleById(c *gin.Context)  {
+	id ,ok:=c.Params.Get("id")
+	if !ok{
+		c.JSON(http.StatusBadRequest,gin.H{"error":"无效的id"})
+	}
+	if err:= service.DeleteUserById(id);err!=nil{
+		c.JSON(http.StatusBadRequest,gin.H{"error":err.Error()})
+	}else {
+		c.JSON(http.StatusOK,gin.H{id:"deleted"})
+	}
+}
